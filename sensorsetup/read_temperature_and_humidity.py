6ac87c8eb51105effc4fd8c954dc9613a515cbfd
Tereza Iofciu <6162692+terezaif@users.noreply.github.com>
2018-12-06 20:37:44 +0100

    Update read_temperature_and_humidity.py
    python3 friendly

--- sensorsetup/read_temperature_and_humidity.py
+++ sensorsetup/read_temperature_and_humidity.py
@@ -6,4 +6,4 @@ pin = 4
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    print ('Temp: {0:0.1f} C  Humidity: {1:0.1f}'.format(temperature, humidity))
