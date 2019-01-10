7c7816ebcf75c9c6a421757e02730588602a0ddf
Tereza Iofciu <t.iofciu@mytaxi.com>
2019-01-10 20:45:27 +0100

    moved seed to file

--- tangle/generate_receiving_addresses.py
+++ tangle/generate_receiving_addresses.py
@@ -1,6 +1,11 @@
 from iota import Iota
 
-api = Iota('https://durian.iotasalad.org:14265', b'THIS9IS9MY9SEED')
+#read the file from the seed file (also change the value of the seed file locally)
+in_file = open("tangle/seed.cfg", "rb") # opening for [r]eading as [b]inary
+seed = in_file.read() # if you only wanted to read 512 bytes, do .read(512)
+in_file.close()
+
+api = Iota('https://durian.iotasalad.org:14265', seed)
 
 # Generate 5 addresses, starting with index 0.
 gna_result = api.get_new_addresses(count=5)
