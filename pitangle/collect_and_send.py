19fd6117d519801842e879822888f4a325adfd7c
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-01-13 23:44:29 +0100

    Use same seed for sending data

--- pitangle/collect_and_send.py
+++ pitangle/collect_and_send.py
@@ -6,20 +6,26 @@ from datetime import datetime
 from datetime import datetime
 from iota import Address, Iota, ProposedTransaction, TryteString
 
-in_file = open("tangle/address_pi.conf", "rb") # opening for [r]eading as [b]inary
-ADDRESS = in_file.read() # if you only wanted to read 512 bytes, do .read(512)
-in_file.close()
+#read the file from the seed file (also change the value of the seed file locally)
+seed_file = open("tangle/seed_tan.conf", "rb") # opening for [r]eading as [b]inary
+seed = seed_file.read() # if you only wanted to read 512 bytes, do .read(512)
+seed_file.close()
+address_file = open("tangle/address_pi.conf", "rb") # opening for [r]eading as [b]inary
+ADDRESS = address_file.read() # if you only wanted to read 512 bytes, do .read(512)
+address_file.close()
 
-api = Iota("https://durian.iotasalad.org:14265")
+api = Iota("https://durian.iotasalad.org:14265", seed)
 receiver = Address(ADDRESS)
 
 sensor = 11
 pin = 4
 
+print("Restarting the process...")
+
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     timestamp = datetime.now()
-    print('Temp: {0:0.1f} C   Humidity: {1:0.1f}   Measured at: {2:s}'.format(temperature, humidity, timestamp.strftime("%Y-%m-%d %H:%M:%S")))
+    print('Temp: {0:0.1f} C   Humidity: {1:0.1f}   Measured at: {2:s}'.format(temperature, humidity, timestamp.strftime("%Y-%m-%d, %H:%M:%S")))
     measurement = {"humidity": humidity, "temperature": temperature, "measuredAt": str(timestamp)}
     message = TryteString.from_string(json.dumps(measurement))
     tx = ProposedTransaction(address=receiver, value=0, message=message)
