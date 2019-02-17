b0c495fba57b7eebebe407b3262f7f20d8c586e1
Avare Stewart <a.stewart@eos-solutions.com>
2019-02-17 19:46:12 +0100

    add analysis folder

--- tangle/send_data.py
+++ tangle/send_data.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from iota import Address, Iota, ProposedTransaction, TryteString
-
-with open("tangle/config/seed.conf") as in_file:
+import os
+with open("tangle/seed.conf") as in_file:
     SEED = in_file.read().rstrip("\n").encode()
 
 
@@ -10,10 +10,29 @@ with open("tangle/config/address_tan.conf") as in_file:
 
 api = Iota("https://durian.iotasalad.org:14265", seed=SEED)
 
-message = TryteString.from_string("Hello PyLadies Hamburg! It's {}".format(str(datetime.now())))
+fname = '/Users/stewarta/repos/getting-started-raspberry-pi/pitangle/current_measurements.csv'
+f = open(fname)
+data = f.read()
+f.close()
 
-receiver = Address(ADDRESS)
+arr = data.split("\n")
+batch_sz = 40
+batch = []
 
-tx = ProposedTransaction(address=receiver, value=0, message=message)
+for i in len(arr):
+    if i%40 == 0:
+        # empty list
+        batch = []
+        # send data
+        send_data(str(batch)
+    else:
+        # append data
+        batch.append(arr[i] + "")
+        batch.append("\n")
 
-api.send_transfer(depth=1, transfers=[tx])
+
+def send_data(data):
+    message = TryteString.from_string(data)
+    receiver = Address(ADDRESS)
+    tx = ProposedTransaction(address=receiver, value=0, message=message)
+    api.send_transfer(depth=1, transfers=[tx])
